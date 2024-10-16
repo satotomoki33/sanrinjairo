9632f7908a8d51c7fbe591d332d302c20b89afa9
Sato Tomoki <mega33420@gmail.com>
2024-10-16 22:11:38 +0900

    syusei ps3

--- Core/Src/cppmain.cpp
+++ Core/Src/cppmain.cpp
@@ -25,7 +25,6 @@ int clamp(int x, int a, int b) {
 }
 
 extern "C" void cppmain() {
-	//HAL_Delay(1000);
 	UART debug(&huart2);
 	UART uart3(&huart3);
 	CAN can(&hfdcan1);
@@ -43,12 +42,12 @@ extern "C" void cppmain() {
 		double vx = ps3.get_axis(PS3::Axis::LEFT_X);
 		double vy = -ps3.get_axis(PS3::Axis::LEFT_Y);
 
-		double R = -bno.get_euler_x();
+		double R = -bno.get_euler_x() ;
 		//double R = 0;
 
-		double v1 = 60 * (vx * cos(0 + R) + vy * sin(0 + R));
-		double v2 = 60 * (vx * cos((M_PI * 2 / 3) + R) + vy * sin((M_PI * 2 / 3)+ R));
-		double v3 = 60
+		double v1 = 80 * (vx * cos(0 + R) + vy * sin(0 + R));
+		double v2 = 80 * (vx * cos((M_PI * 2 / 3) + R) + vy * sin((M_PI * 2 / 3)+ R));
+		double v3 = 80
 				* (vx * cos((M_PI * 4 / 3) + R) + vy * sin((M_PI * 4 / 3) + R));
 
 		if (ps3.get_key(PS3::Key::L1)) {
@@ -65,20 +64,19 @@ extern "C" void cppmain() {
 
 		double K = 100;
 		double e1 = v1 - c610.get_rps(C610::ID::ID1);
-		int c1 = clamp(K * e1, -2500, 2500);
+		int c1 = clamp(K * e1, -4000, 4000);
 
 		double e2 = v2 - c610.get_rps(C610::ID::ID2);
-		int c2 = clamp(K * e2, -2500, 2500);
+		int c2 = clamp(K * e2, -4000, 4000);
 
 		double e3 = v3 - c610.get_rps(C610::ID::ID3);
-		int c3 = clamp(K * e3, -2500, 2500); //P制御
+		int c3 = clamp(K * e3, -4000, 4000); //P制御
 
 		c610.set_current(C610::ID::ID1, c1);
 		c610.set_current(C610::ID::ID2, c2);
 		c610.set_current(C610::ID::ID3, c3); //モーターを動かす
 
-		c610.transmit(); //3つのモーターの指示
-
+		c610.transmit();
 		//debug.printf("%f\t%f\t%f\t%f\t%f\r\n", vx, vy, v1, v2, v3);
 	});
 
